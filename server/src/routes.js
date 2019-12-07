b706f4a2086bb9f360bbd01d22771b774e62926c
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-07 16:34:24 -0300

    feat: add auth with jwt

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/Auth';
+
+import SessionController from './app/controllers/AuthController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to Gympoint' }));
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/students', (req, res) => {
+  return res.send('aa');
+});
 
 export default routes;
