1579bf25b895792686069b76197a9b5c61a4ce76
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-08 02:44:13 -0300

    feat: add plans features

---
+++ server/src/app/controllers/PlanController.js
@@ -0,0 +1,113 @@
+import * as Yup from 'yup';
+
+import Plan from '../models/Plan';
+
+class PlanController {
+  async index(req, res) {
+    /*
+     * Função que lista planos cadastrados
+     */
+
+    const plans = await Plan.findAll();
+
+    return res.status(200).json({ plans });
+  }
+
+  async store(req, res) {
+    /*
+     * Função que registra um novo plano
+     */
+
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!schema.isValid(req.body)) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const planExists = await Plan.findOne({ where: { title: req.body.title } });
+
+    if (planExists) {
+      return res.status(401).json({ error: 'Plan already exists' });
+    }
+
+    const { id, title, duration, price } = await Plan.create(req.body);
+
+    return res.status(200).json({ id, title, duration, price });
+  }
+
+  async update(req, res) {
+    /*
+     * Função que atualiza planos cadastrados com base no id
+     */
+
+    const schemaBody = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number(),
+      price: Yup.number(),
+    });
+
+    const schemaParams = Yup.object().shape({
+      id: Yup.number().required(),
+    });
+
+    if (
+      !(await schemaBody.isValid(req.body)) ||
+      !(await schemaParams.isValid(req.params))
+    ) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const plan = await Plan.findByPk(req.params.id);
+
+    if (!plan) {
+      return res.status(401).json({ error: 'Unable to find this plan' });
+    }
+
+    if (req.body.title) {
+      const { title } = req.body;
+      if (title !== plan.title) {
+        const planExists = await Plan.findOne({ where: { title } });
+
+        if (planExists) {
+          return res.status(401).json({ error: 'Plan already exists' });
+        }
+      }
+    }
+
+    await plan.update(req.body);
+
+    const { id, title, duration, price } = await Plan.findByPk(req.params.id);
+
+    return res.status(200).json({ id, title, duration, price });
+  }
+
+  async delete(req, res) {
+    /*
+     * Função que remove um plano a partir do seu id
+     */
+
+    const schema = Yup.object().shape({
+      id: Yup.number(),
+    });
+
+    if (!schema.isValid(req.params)) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const plan = await Plan.findByPk(req.params.id);
+
+    if (!plan) {
+      return res.status(401).json({ error: 'Could not find this plan' });
+    }
+
+    await Plan.destroy({ where: { id: req.params.id } });
+
+    return res.status(200).json({ message: 'Plan successful deleted' });
+  }
+}
+
+export default new PlanController();
--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,7 @@ import authMiddleware from './app/middlewares/Auth';
 
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 const routes = new Router();
 
@@ -12,7 +13,14 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.get('/students', StudentController.index);
 routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
+routes.delete('/students/:id', StudentController.delete);
+
+routes.get('/plans', PlanController.index);
+routes.post('/plans', PlanController.store);
+routes.put('/plans/:id', PlanController.update);
+routes.delete('/plans/:id', PlanController.delete);
 
 export default routes;
