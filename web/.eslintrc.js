68a9bb259efa926079fb4e0341f90f38051e350f
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-13 10:15:37 -0300

    feat: add header component

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -31,7 +31,8 @@ module.exports = {
     camelcase: "off",
     "no-console": ["error", { allow: ["tron"] }],
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "jsx-a11y/click-events-have-key-events": "off"
   },
   settings: {
     "import/resolver": {
