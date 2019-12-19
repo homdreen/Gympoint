feec1eacd0b91efce297cd6b5ad986706395406e
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-19 19:50:26 -0300

    feat: add init checkin page

--- mobile/src/pages/Checkin/index.js
+++ mobile/src/pages/Checkin/index.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import { formatDistance, parseISO } from 'date-fns';
+import { pt } from 'date-fns/locale';
+
+import { useSelector } from 'react-redux';
 
 import Icon from '@expo/vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import { Container, CheckinButton } from './styles';
 
 export default function Checkin() {
+  const [checkins, setCheckins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const studentId = useSelector(state => state.user.id);
+
+  useEffect(() => {
+    async function loadCheckins() {
+      setLoading(true);
+      try {
+        const response = await api.get(`/students/${studentId}/checkins`);
+
+        const data = response.data.map(checkin => ({
+          ...checkin,
+          dateFormatted: formatDistance(
+            parseISO(checkin.createdAt),
+            new Date(),
+            {
+              addSuffix: true,
+              locale: pt,
+            }
+          ),
+        }));
+
+        setCheckins(data);
+      } catch (err) {
+        Alert.alert(
+          'Falha!',
+          'Não foi possível recuperar check-ins já feitos!'
+        );
+      }
+      setLoading(false);
+    }
+
+    loadCheckins();
+  }, [studentId]);
+
   return (
     <>
       <Header />
+
+      <Container>
+        <CheckinButton loading={loading}>Novo check-in</CheckinButton>
+      </Container>
     </>
   );
 }
---
+++ mobile/src/pages/Checkin/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+import Button from '~/components/Button';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 30px;
+  margin-top: 20px;
+`;
+
+export const CheckinButton = styled(Button)`
+  align-self: stretch;
+`;
